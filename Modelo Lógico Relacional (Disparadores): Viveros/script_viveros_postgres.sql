dc687bda281a5852638f2ee14d3d6caa0c9711a3
alu0101214627 <92092409+alu0101214627@users.noreply.github.com>
2021-11-04 12:40:40 +0100

    Add files via upload
    Función crear_email y trigger funcionales correctamente

--- Modelo Lógico Relacional (Disparadores): Viveros/script_viveros_postgres.sql
+++ Modelo Lógico Relacional (Disparadores): Viveros/script_viveros_postgres.sql
@@ -28,13 +28,13 @@ CREATE TABLE viveros.zona (
 );
 
 CREATE TABLE viveros.empleado (
-    dni VARCHAR(9) NOT NULL,
+    DNI VARCHAR(9) NOT NULL,
     sueldo FLOAT NOT NULL,
     telefono INT NOT NULL,
     zona_identificador INT NOT NULL,
     zona_latitud_vivero FLOAT NOT NULL,
     zona_longitud_vivero FLOAT NOT NULL,
-    PRIMARY KEY (dni),
+    PRIMARY KEY (DNI),
     CONSTRAINT fk_zona_identificador
     FOREIGN KEY (zona_identificador)
     REFERENCES viveros.zona (identificador)
@@ -56,7 +56,8 @@ CREATE TABLE IF NOT EXISTS viveros.producto (
   codigo INT NOT NULL UNIQUE,
   stock INT,
   precio FLOAT,
-  PRIMARY KEY (codigo));
+  PRIMARY KEY (codigo)
+);
 
 
 CREATE TABLE IF NOT EXISTS viveros.cliente_club (
@@ -66,6 +67,7 @@ CREATE TABLE IF NOT EXISTS viveros.cliente_club (
   nombre VARCHAR(20),
   apellido1 VARCHAR(25),
   apellido2 VARCHAR(25),
+  email VARCHAR(45),
   PRIMARY KEY (DNI));
 
 
@@ -123,42 +125,42 @@ CREATE TABLE IF NOT EXISTS viveros.producto_has_zona (
     ON UPDATE CASCADE
 );
 
-CREATE OR REPLACE FUNCTION crear_email--(IN dominio varchar)
-  RETURNS TRIGGER AS
-$$
+CREATE OR REPLACE FUNCTION crear_email()
+  RETURNS trigger AS $email$
 DECLARE
-    email VARCHAR(45);
+    correo VARCHAR(45);
 BEGIN
-    email := CONCAT (dni, '@', TG_ARGV[0]);
-    IF nombre IS NOT NULL THEN
-        IF apellido1 IS NOT NULL THEN
-            email := CONCAT (nombre, apellido1, '@', TG_ARGV[0]);
+    correo := CONCAT (NEW.DNI, '@', TG_ARGV[0]);
+    IF NEW.nombre IS NOT NULL THEN
+        IF NEW.apellido1 IS NOT NULL THEN
+            correo := CONCAT (NEW.nombre, NEW.apellido1, '@', TG_ARGV[0]);
         ELSE
-            IF apellido2 IS NOT NULL THEN
-                email := CONCAT (nombre, apellido2, '@', TG_ARGV[0]);
+            IF NEW.apellido2 IS NOT NULL THEN
+                correo := CONCAT (NEW.nombre, NEW.apellido2, '@', TG_ARGV[0]);
             ELSE
-                RAISE NOTICE 'creado correo por defecto %', now();
+                RAISE NOTICE 'creado correo por defecto';
             END IF;
         END IF;
     ELSE 
-        IF apellido1 IS NOT NULL THEN
-            IF apellido2 IS NOT NULL THEN
-                email := CONCAT (apellido1, apellido2, '@', TG_ARGV[0]);
+        IF NEW.apellido1 IS NOT NULL THEN
+            IF NEW.apellido2 IS NOT NULL THEN
+                correo := CONCAT (NEW.apellido1, NEW.apellido2, '@', TG_ARGV[0]);
             ELSE
-                RAISE NOTICE 'creado correo por defecto %', now();
+                RAISE NOTICE 'creado correo por defecto';
             END IF;
         ELSE
-            RAISE NOTICE 'creado correo por defecto %', now();
+            RAISE NOTICE 'creado correo por defecto';
         END IF;
     END IF;
-    RETURN email;
+    NEW.email = correo;
+    RETURN NEW;
 END;
-$$
-LANGUAGE 'plpgsql';
+$email$ LANGUAGE 'plpgsql';
 
 CREATE TRIGGER trigger_crear_email_before_insert
   BEFORE INSERT
   ON cliente_club
+  FOR EACH ROW
   EXECUTE PROCEDURE crear_email('ull.edu.es');
 
 INSERT INTO viveros.vivero (latitud, longitud, localidad) values (28.5225, -16.337944 , 'San Cristóbal de La Laguna');
@@ -167,9 +169,9 @@ INSERT INTO viveros.vivero (latitud, longitud, localidad) values (28.3556667, -1
 INSERT INTO viveros.zona (identificador, latitud_vivero, longitud_vivero) values (1, 28.5225, -16.337944);
 INSERT INTO viveros.zona (identificador, latitud_vivero, longitud_vivero) values (2, 28.3556667, -16.370583);
 
-INSERT INTO viveros.empleado (dni, sueldo, telefono, zona_identificador, zona_latitud_vivero, zona_longitud_vivero) values ('02203658A', 1000, 614572983, 1, 28.5225, -16.337944);
-INSERT INTO viveros.empleado (dni, sueldo, telefono, zona_identificador, zona_latitud_vivero, zona_longitud_vivero) values ('09841512B', 1000, 614572984, 1, 28.3556667, -16.370583);
-INSERT INTO viveros.empleado (dni, sueldo, telefono, zona_identificador, zona_latitud_vivero, zona_longitud_vivero) values ('89645151C', 1000, 614572985, 1, 28.3556667, -16.370583);
+INSERT INTO viveros.empleado (DNI, sueldo, telefono, zona_identificador, zona_latitud_vivero, zona_longitud_vivero) values ('02203658A', 1000, 614572983, 1, 28.5225, -16.337944);
+INSERT INTO viveros.empleado (DNI, sueldo, telefono, zona_identificador, zona_latitud_vivero, zona_longitud_vivero) values ('09841512B', 1000, 614572984, 1, 28.3556667, -16.370583);
+INSERT INTO viveros.empleado (DNI, sueldo, telefono, zona_identificador, zona_latitud_vivero, zona_longitud_vivero) values ('89645151C', 1000, 614572985, 1, 28.3556667, -16.370583);
 
 INSERT INTO viveros.producto (codigo, stock, precio) values (0000, 20, 13.63);
 INSERT INTO viveros.producto (codigo, stock, precio) values (0001, 10, 17.61);
@@ -178,11 +180,11 @@ INSERT INTO viveros.producto (codigo, stock, precio) values (0003, 22, 4.99);
 INSERT INTO viveros.producto (codigo, stock, precio) values (0004, 17, 10.99);
 INSERT INTO viveros.producto (codigo, stock, precio) values (0005, 3, 6.31);
 
-INSERT INTO viveros.cliente_club (DNI, credito_mensual, bonificacion) values ('89465132Z', 34.65, 4.35);
-INSERT INTO viveros.cliente_club (DNI, credito_mensual, bonificacion) values ('89465133Y', 35.45, 5.25);
-INSERT INTO viveros.cliente_club (DNI, credito_mensual, bonificacion) values ('89465134X', 36.35, 6.55);
-INSERT INTO viveros.cliente_club (DNI, credito_mensual, bonificacion) values ('89465135W', 37.25, 7.15);
-INSERT INTO viveros.cliente_club (DNI, credito_mensual, bonificacion) values ('89465136V', 39.15, 9.15);
+INSERT INTO viveros.cliente_club (DNI, credito_mensual, bonificacion, nombre, apellido1, apellido2, email) values ('89465132Z', 34.65, 4.35, 'Jorge', 'Ronco', 'Moratilla', NULL);
+INSERT INTO viveros.cliente_club (DNI, credito_mensual, bonificacion, nombre, apellido1, email) values ('89465133Y', 35.45, 5.25, 'Irene', 'Hernandez', NULL);
+INSERT INTO viveros.cliente_club (DNI, credito_mensual, bonificacion, apellido1, apellido2, email) values ('89465134X', 36.35, 6.55, 'Sanz', 'Herranz', NULL);
+INSERT INTO viveros.cliente_club (DNI, credito_mensual, bonificacion, email) values ('89465135W', 37.25, 7.15, NULL);
+INSERT INTO viveros.cliente_club (DNI, credito_mensual, bonificacion, nombre, apellido2, email) values ('89465136V', 39.15, 9.15, 'Cristina', 'Batista', NULL);
 
 INSERT INTO viveros.cliente_club_pedido_empleado (DNI_cliente_club, DNI_empleado, codigo_producto, fecha, cantidad) values ('89465132Z', '89645151C', 0004, '2021-06-21', 3);
 INSERT INTO viveros.cliente_club_pedido_empleado (DNI_cliente_club, DNI_empleado, codigo_producto, fecha, cantidad) values ('89465136V', '09841512B', 0000, '2021-03-10', 4);
