fe524c4fe8998c0d5a88088b416722232906a90e
alu0101214627 <92092409+alu0101214627@users.noreply.github.com>
2021-11-16 09:46:34 +0000

    Códigos finales de los script sobre triggers

---
+++ Modelo Lógico Relacional (Disparadores): Viveros/tests.sql
@@ -0,0 +1,13 @@
+SELECT localidad FROM viveros.vivero;
+
+SELECT latitud_vivero, longitud_vivero FROM viveros.zona;
+
+SELECT dni, telefono, municipio, viviendas FROM viveros.empleado;
+
+SELECT codigo, stock FROM viveros.producto ORDER BY codigo;
+
+SELECT DNI, credito_mensual, email FROM viveros.cliente_club;
+
+SELECT * FROM viveros.cliente_club_pedido_empleado;
+
+SELECT * FROM viveros.producto_has_zona;
